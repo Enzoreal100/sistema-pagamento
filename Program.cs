ec1bd3a0356be697fce50ca97c150ba26ac63e3b
enzoreal100 <enzoreal100@gmail.com>
2026-04-10 20:13:23 -0300

    feat: added menu

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using model.MenuModel;
+
+Menu.StartMenu();
---
+++ model/MenuModel/Menu.cs
@@ -0,0 +1,40 @@
+namespace model.MenuModel;
+
+public static class Menu
+{
+    public static void StartMenu()
+    {
+        Console.WriteLine("========= SISTEMA DE PAGAMENTO ==============");
+        Console.WriteLine("Selecione uma forma de pagamento:");
+        // Print seleção de formas de pagamento
+        
+        while (true)
+        {
+            String userInput = "";
+            Console.WriteLine("1 - Cartão \n" +
+                            "2 - Boleto \n" +
+                            "3 - Sair \n");
+            userInput = Console.ReadLine();
+            if(userInput == "1")
+            {
+                Console.WriteLine("\nMétodo escolhido: Cartão\n");
+                // chama metodo pagamento cartao
+            }
+            else if (userInput == "2")
+            {
+                Console.WriteLine("Método escolhido: Cartão");
+                // chama metodo pagamento boleto
+            }
+            else if (userInput == "3")
+            {
+                Console.WriteLine("Obrigado por utilizar!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Selecione uma das opções disponíveis");
+                continue;
+            }
+        }
+    }
+}
