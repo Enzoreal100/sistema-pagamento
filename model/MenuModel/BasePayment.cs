1796f013416090fdc9a918619030fcfa2e9a3dbe
enzoreal100 <enzoreal100@gmail.com>
2026-04-12 23:03:20 -0300

    feat: implement BasePayment class for payment processing

---
+++ model/MenuModel/BasePayment.cs
@@ -0,0 +1,21 @@
+using sistema_pagamento.Interfaces;
+
+namespace sistema_pagamento.Model;
+
+public abstract class BasePayment : IPayment
+{
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    
+    public BasePayment(decimal amount)
+    {
+        Amount = amount;
+        Date = DateTime.Now;
+    }
+
+    public abstract string ProcessPayment();
+    public string ReciptEmission()
+    {
+        return $"Recibo: \nValor: {Amount} \nData: {Date}";
+    }
+}
